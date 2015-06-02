6efd0e307126347216bcd352afa8d9ec0300958b
Liang Bo Wang <ccwang002@gmail.com>
2015-06-02 11:45:20 +0800

    update raw processing

--- 0_microarray_rawdata_from_GEO.R
+++ 0_microarray_rawdata_from_GEO.R
@@ -2,18 +2,29 @@
 # biocLite(c("GEOquery", "affy", "limma"))
 library(GEOquery)
 
+# Download raw data from GEO database
 getGEOSuppFiles("GSE10940", baseDir = "./GEO_datasets/")
+# Extract the CEL files by the following commands (already done)
 # tar xf GSE10940_RAW.tar
 # gunzip [--keep] *.gz
+
+# Read in the published array data (processed probe values)
 gse10940 <- getGEO(
     "GSE10940", GSEMatrix = FALSE,
     destdir = "./GEO_datasets/"
 )
+# Get the description about each samples(GSM files)
 gse10940_pheno_tab <- pData(phenoData(gse10940[[1]]))
 gse10940_pheno_tab[, c("title", "type", "source_name_ch1")]
-gpl <- getGEO(annotation(gse10940[[1]]), destdir = "./GEO_datasets/")
-cat(Meta(gpl)$title)
 
+# Get the platform of each samples
 gsmplatforms <- lapply(GSMList(gse10940), function(x){Meta(x)$platform})
+
+# Get the platform information
+gpl <- getGEO(gsmplatforms[[1]], destdir = "./GEO_datasets/")
+cat(Meta(gpl)$title)  # Print out the platform name
+
+# Get published probe data
 pub_probe_val <- Table(GSMList(gse10940)[[1]])
-Columns(GSMList(gse10940)[[1]])
+Columns(GSMList(gse10940)[[1]])  # Get the description for each column
+
